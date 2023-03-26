d0c5e984ee9d84092f9539eed2ade8b8bbd56923
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-03-26 23:09:28 +0300

    Blockers faced

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+// .then(response => response.json())
+// .then(object => console.log(object))
+// .catch(error => {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+// });
+
+function submitData(userName, userEmail) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+          }),
+    };
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(object => {
+        document.body.innerHTML = object["id"];
+        console.log(object);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+    
+}
+
+// submitData("Jim", "jim@jim.com");
